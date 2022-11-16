62031eafe024d73cf3aa16ac8cbd88233c64c7f7
AntoineD01 <94221570+AntoineD01@users.noreply.github.com>
2022-11-16 17:51:26 +0100

    Create Generator.h

---
+++ Generator.h
@@ -0,0 +1,15 @@
+//
+// Created by Antoine Dupont on 13/11/2022.
+//
+
+#ifndef TEST_PROJET_GENERATOR_H
+#define TEST_PROJET_GENERATOR_H
+
+#include "Trees.h"
+#include "functions.h"
+#include <time.h>
+
+void menu(t_tree N, t_tree V, t_tree A, t_tree Av);
+void generator(t_tree N, t_tree V, t_tree A, t_tree Av);
+int random (int n);
+#endif //TEST_PROJET_GENERATOR_H
