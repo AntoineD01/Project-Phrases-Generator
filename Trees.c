5e7c3bf23011ee1b9662645ac8bd88b3b7ca8c96
AntoineD01 <94221570+AntoineD01@users.noreply.github.com>
2022-11-20 21:57:03 +0100

    Update Trees.c

--- Trees.c
+++ Trees.c
@@ -3,15 +3,13 @@
 //
 #include "Trees.h"
 
-t_node* new_values(char letter, t_tree *Tree, t_node* leaf)
+t_node* new_values(char letter, t_tree *Tree, t_node* leaf) //Add nodes to the tree and return it
 {
     t_node* n_node;
     n_node = malloc(sizeof(t_node));
-    //printf("First empty spot of %c : %d\n",leaf->letter,empty_spot(*leaf));
     if (empty_spot(*leaf) == 0) //If no child yet
     {
-        //printf("This letter has no child yet\n");
-        leaf->f_letters[0] = n_node;
+        leaf->f_letters[0] = n_node; //The first child of the leaf will be this node
         n_node->letter = letter;
         for (int i=0; i<26; i++)
         {
@@ -34,8 +32,8 @@ t_node* new_values(char letter, t_tree *Tree, t_node* leaf)
         }
         if (exist == 0) //If the letter is not in the tree
         {
-            //printf("The letter is not in the tree \n");
-            leaf->f_letters[empty_spot(*leaf)] = n_node;
+            printf("The letter is not in the tree.\n");
+            leaf->f_letters[empty_spot(*leaf)] = n_node; //Add the node in the first empty pointer
             n_node->letter = letter;
             for (int i=0; i<26; i++)
             {
@@ -45,20 +43,20 @@ t_node* new_values(char letter, t_tree *Tree, t_node* leaf)
         }
         else //If the letter is already in the tree
         {
-            //printf("The letter is in the tree\n");
+            printf("The letter is in the tree.\n");
             free(n_node);
-            return leaf->f_letters[index];
+            return leaf->f_letters[index]; //In this case we return the node that have the letter we have been working on
         }
     }
 }
 
 
-void display_node(t_node n)
+void display_node(t_node n) //Display the node and its children
 {
-    printf("The letter in this node is %c and have %d child.\n\n",n.letter, empty_spot(n));
+    printf("The letter in this node is %c and have %d child.\n\n",n.letter, empty_spot(n)); //Display the node
     for (int i=0; i< empty_spot(n); i++)
     {
-        printf("The value at index %d is %c\n",i,n.f_letters[i]->letter);
+        printf("The value at index %d is %c.\n",i,n.f_letters[i]->letter); //Display children of the node
     }
 }
 
@@ -73,12 +71,12 @@ int empty_spot(t_node n) //Find the first empty spot in the pointer spot
     return i;
 }
 
-void information(t_node* n, word A)
+void information(t_node* n, word A) //Put the information of the word in the right spot of the structure
 {
     word* infos= malloc(sizeof(word));
     flexed* fle = malloc(sizeof(flexed));
     infos->nature = A.nature;
-    if (infos->nature == 1 || infos->nature == 3) //Nom and Adjectif
+    if (infos->nature == 1 || infos->nature == 3) //Noun and Adjective
     {
         infos->genre = A.genre;
         infos->nombre = A.nombre;
@@ -87,15 +85,15 @@ void information(t_node* n, word A)
     }
     else
     {
-        if(infos->nature == 2) //Verbe
+        if(infos->nature == 2) //Verb
         {
             infos->temps = A.temps;
             fle->himself = infos;
             n->f = fle;
         }
-        else //Adverbe
+        else
         {
-            if(infos->nature == 4)
+            if(infos->nature == 4) //Adverb
             {
                 infos->nombre = A.nombre;
                 fle->himself = infos;
@@ -114,14 +112,13 @@ void information(t_node* n, word A)
     }
 }
 
-t_node* research(word A,t_tree t)
+t_node* research(word A,t_tree t) //Look for a word in the tree and return a pointer to it last node
 {
     t_node* temp;
-    temp = malloc(sizeof(t_node));
     int index=0;
     int ctr=0;
     temp = t.root;
-    if (temp->f_letters[0] == NULL)
+    if (temp->f_letters[0] == NULL) //If the tree has no node
     {
         printf("This word is not in the tree.\n");
         return 0;
@@ -131,7 +128,7 @@ t_node* research(word A,t_tree t)
     {
         for (int i=0; i<empty_spot(*temp); i++)
         {
-            if (A.base_word[index] == temp->f_letters[i]->letter && empty_spot(*temp) != 0 && index!=strlen(A.base_word))
+            if (A.base_word[index] == temp->f_letters[i]->letter && empty_spot(*temp) != 0 && index!=strlen(A.base_word)) //Check if every letter are the letter of the word we search for
             {
                 temp = temp->f_letters[i];
                 index++;
@@ -144,7 +141,7 @@ t_node* research(word A,t_tree t)
         }
     }
 
-    if (ctr == strlen(A.base_word))
+    if (ctr == strlen(A.base_word)) //If the counter is as long as the length of the word we are searching
     {
         printf("This word is in the tree.\n\n");
         return temp;
@@ -156,9 +153,12 @@ t_node* research(word A,t_tree t)
     }
 }
 
-void forme_flechi(word A, t_node* n) //TO BE DONE
+void forme_flechi(word A, t_node* n) //This function did not work and that's why it is not used in the main
 {
-    int index=0;
+    printf("The letter is %c.",n->letter);
+
+    int index = 0;
+
     if(n->f->f_flechie[index] == NULL)
     {
         printf("%d",index);
@@ -166,7 +166,6 @@ void forme_flechi(word A, t_node* n) //TO BE DONE
     }
     else
     {
-        printf("The first empty spot is %d",index);
+        printf("The first empty spot is %d.",index);
     }
 }
-
